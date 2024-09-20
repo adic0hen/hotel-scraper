04dd6b3abe6155242ebfd5eb961e26f6ef268565
Or Aharonee <17099688+Aharonee@users.noreply.github.com>
2024-09-20 23:22:47 +0300

    fix env var

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const kv = await Deno.openKv();
 const botToken = Deno.env.get("TELEGRAM_BOT_TOKEN");
-const telegramApiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}`
+const telegramApiUrl = `https://api.telegram.org/bot${botToken}`
 const chatId = "-826488742";
 
 Deno.cron("Increment a counter", "*/5 * * * *", async () => {
