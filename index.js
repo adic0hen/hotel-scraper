c2d669ee3ddeea0cbc4f985dcb452c42fd783fd7
Or Aharonee <17099688+Aharonee@users.noreply.github.com>
2024-09-21 00:36:12 +0300

    remove redundant logging

--- index.js
+++ index.js
@@ -3,30 +3,22 @@ const botToken = Deno.env.get("TELEGRAM_BOT_TOKEN");
 const telegramApiUrl = `https://api.telegram.org/bot${botToken}`
 const chatId = "-826488742";
 
-Deno.cron("Scrape Booking", "*/5 * * * *", async () => {
+Deno.cron("Scrape Booking", "*/10 * * * *", async () => {
     // increment a count using Deno KV
     // await kv.atomic().sum(["task_runs"], 1n).commit();
-    try {
-        const response = await fetch(telegramApiUrl + '/sendMessage', {
-            method: "POST",
-            body: JSON.stringify({
-                chat_id: chatId,
-                text: "cronjob running"
-            }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        console.log("Telegram API response status:", response.status);
-        const responseData = await response.json();
-        console.log("Telegram API response data:", JSON.stringify(responseData, null, 2));
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+    const response = await fetch(telegramApiUrl + '/sendMessage', {
+        method: "POST",
+        body: JSON.stringify({
+            chat_id: chatId,
+            text: "cronjob running"
+        }),
+        headers: {
+            "Content-Type": "application/json"
         }
-    } catch (error) {
-        console.error("Error sending message to Telegram:", error.message);
+    })
+
+    if (!response.ok) {
+        console.error("Error sending message to Telegram:", response.status);
     }
 });
 
