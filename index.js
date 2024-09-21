a89f0b59fa9eeb8b40e5c2b3b75731f13ee62ea9
Or Aharonee <17099688+Aharonee@users.noreply.github.com>
2024-09-22 01:09:50 +0300

    Add Kurokawa query

--- index.js
+++ index.js
@@ -8,13 +8,18 @@ const chatId = "-1002395888989";
 const searchQueries = [
     {
         name: "Tokyo (start of trip)",
-        url: 'https://www.booking.com/searchresults.en-gb.html?label=en-il-booking-desktop-sD66O*sLIyR37D38rZhp7wS652796016630%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp1008006%3Ali%3Adec%3Adm&sid=57825a18ad110af452ea4c6eee11226e&aid=2311236&ss=Shinjuku+Area%2C+Tokyo%2C+Tokyo-to%2C+Japan&ssne=Hakone&ssne_untouched=Hakone&highlighted_hotels=317205&efdco=1&lang=en-gb&src=searchresults&dest_id=3054&dest_type=district&ac_position=0&ac_click_type=b&ac_langcode=en&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=013a5924bd560362&checkin=2024-10-17&checkout=2024-10-22&group_adults=2&no_rooms=1&group_children=0&nflt=price%3DILS-min-700-1%3Bfc%3D2%3Breview_score%3D80&selected_currency=ILS',
+        url: 'https://www.booking.com/searchresults.en-gb.html?label=en-il-booking-desktop-sD66O*sLIyR37D38rZhp7wS652796016630%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp1008006%3Ali%3Adec%3Adm&sid=57825a18ad110af452ea4c6eee11226e&aid=2311236&ss=Shinjuku+Area%2C+Tokyo%2C+Tokyo-to%2C+Japan&ssne=Hakone&ssne_untouched=Hakone&highlighted_hotels=317205&efdco=1&lang=en-gb&src=searchresults&dest_id=3054&dest_type=district&ac_position=0&ac_click_type=b&ac_langcode=en&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=013a5924bd560362&checkin=2024-10-17&checkout=2024-10-22&group_adults=2&no_rooms=1&group_children=0&nflt=price%3DILS-min-600-1%3Bfc%3D2%3Breview_score%3D80&selected_currency=ILS',
         nights: 5
     },
     {
         name: "Hakone",
         url: 'https://www.booking.com/searchresults.en-gb.html?label=en-il-booking-desktop-sD66O*sLIyR37D38rZhp7wS652796016630%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp1008006%3Ali%3Adec%3Adm&sid=57825a18ad110af452ea4c6eee11226e&aid=2311236&ss=Hakone&ssne=Hakone&ssne_untouched=Hakone&highlighted_hotels=317205&efdco=1&lang=en-gb&src=searchresults&dest_id=-228233&dest_type=city&checkin=2024-10-22&checkout=2024-10-24&group_adults=2&no_rooms=1&group_children=0&nflt=review_score%3D80%3Broomfacility%3D38%3Boos%3D1%3Bfc%3D2%3Bprice%3DILS-min-1200-1&selected_currency=ILS',
         nights: 2
+    },
+    {
+        name: "Kurokawa Onsen",
+        url: 'https://www.booking.com/searchresults.en-gb.html?label=gen173nr-1BCAEoggI46AdIM1gEaGqIAQGYAQm4ARfIAQzYAQHoAQGIAgGoAgO4AojnvLcGwAIB0gIkMjIwYjc0YTQtNThjMC00MzYzLWE1NTQtNTMwNjk0MGU5NGFl2AIF4AIB&sid=8a9e3908c14e4f5341bb7e7d2f31a3c0&aid=304142&ss=Kurokawa+Onsen%2C+Minamioguni%2C+Kumamoto%2C+Japan&ssne=Beppu&ssne_untouched=Beppu&highlighted_hotels=585298&efdco=1&lang=en-gb&src=hotel&dest_id=7021&dest_type=district&ac_position=0&ac_click_type=b&ac_langcode=en&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=5cb793a53c520284&checkin=2024-10-31&checkout=2024-11-01&group_adults=2&no_rooms=1&group_children=0&nflt=review_score%3D80%3Broomfacility%3D38%3Bht_id%3D209%3Boos%3D1%3Bfc%3D2%3Bprice%3DILS-min-1500-1&selected_currency=ILS',
+        nights: 1
     }
 ]
 
